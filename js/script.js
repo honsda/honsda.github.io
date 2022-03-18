d8ed92ccbcc59d11b1ed558a0ca562aa67421baf
hons <70585552+honsda@users.noreply.github.com>
2022-03-18 03:26:43 -0700

    stuff

--- js/script.js
+++ js/script.js
@@ -3,10 +3,3 @@ function mAutoplay() {
     aud_play_pause();
     document.removeEventListener('click', mAutoplay);
 }
-
-anime({
-    targets: '.snowflake',
-    rotate: anime.stagger([-360, 360]),
-    easing: 'easeInOutQuad',
-    loop: true
-});
