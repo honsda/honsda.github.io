4ec01175b8894a550b0521f9efa640686003d64d
honsda <xuehonia@gmail.com>
2026-03-04 05:09:45 +0700

    feat: add GitHub Actions workflow for deployment and create .nojekyll files

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://honsda.github.io',
   integrations: [svelte()],
 
   vite: {
